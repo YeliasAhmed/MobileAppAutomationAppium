4a43237458de5ba76fc49d92518a1e2700867c83
Yelias Ahmed <yeliasahmed89@gmail.com>
2023-06-29 04:29:08 +0600

    Updated

--- src/test/java/appiumMobileTest/Calculator.java
+++ src/test/java/appiumMobileTest/Calculator.java
@@ -1,43 +1,38 @@
 package appiumMobileTest;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.URL;
 
-public class Calender {
+public class Calculator {
 
 
-     static AppiumDriver driver;
+     public static AppiumDriver<MobileElement> driver;
 
-    public static void openSmartScale() throws Exception {
+    public static void calculatorOpen() throws Exception {
 
 //        Device Configuration
 
         DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setCapability("deviceName", "Redmi 7");
+        cap.setCapability("deviceName", "Xiaomi Redmi 7");
         cap.setCapability("udid", "fcaef21b");
         cap.setCapability("platformName", "Android");
-        cap.setCapability("platformVersion", "11");
-
-// Apps Configuration
-
-
+        cap.setCapability("platformVersion", "10");
+ // Apps Configuration
         cap.setCapability("appPackage", "com.miui.calculator");
         cap.setCapability("appActivity", "com.miui.calculator.cal.CalculatorActivity");
 
-
 //        Appium Driver
-
-        URL url = new URL("http://127.0.0.0:4723/");
-        driver = new AppiumDriver(url, cap);
-
-        System.out.println("Teton Smart Scale is Started...");
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
+        driver = new AppiumDriver<MobileElement>(url, cap);
+        System.out.println("Calculator is started...");
 
     }
     public static void main(String[] args) throws Exception {
-        openSmartScale();
+        calculatorOpen();
 
        try {
-           openSmartScale();
+           calculatorOpen();
        }catch (Exception e){
            System.out.println("Device not found");
            System.out.println("Device not found");
