e1cb0c11dbad52d904611f81d7430951a9a7d493
Yelias Ahmed <yeliasahmed89@gmail.com>
2023-07-01 04:16:05 +0600

    Updated

--- src/test/java/appiumMobileTest/Calculator.java
+++ src/test/java/appiumMobileTest/Calculator.java
@@ -1,13 +1,18 @@
 package appiumMobileTest;
+
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
 import java.net.URL;
 
 public class Calculator {
 
 
-     public static AppiumDriver<MobileElement> driver;
+    public static AppiumDriver<MobileElement> driver;
+
 
     public static void calculatorOpen() throws Exception {
 
@@ -18,24 +23,39 @@ public class Calculator {
         cap.setCapability("udid", "fcaef21b");
         cap.setCapability("platformName", "Android");
         cap.setCapability("platformVersion", "10");
- // Apps Configuration
-        cap.setCapability("appPackage", "com.miui.calculator");
-        cap.setCapability("appActivity", "com.miui.calculator.cal.CalculatorActivity");
+        // Apps Configuration
+        cap.setCapability("appPackage", "com.google.android.calculator");
+        cap.setCapability("appActivity", "com.android.calculator2.Calculator");
+
 
 //        Appium Driver
         URL url = new URL("http://127.0.0.1:4723/wd/hub");
         driver = new AppiumDriver<MobileElement>(url, cap);
         System.out.println("Calculator is started...");
 
-    }
-    public static void main(String[] args) throws Exception {
-        calculatorOpen();
 
-       try {
-           calculatorOpen();
-       }catch (Exception e){
-           System.out.println("Device not found");
-           System.out.println("Device not found");
-       }
+
+        driver.findElement(By.id("com.google.android.calculator:id/digit_5")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/op_mul")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/digit_8")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/eq")).click();
+        MobileElement result = driver.findElement(By.className("android.widget.TextView"));
+        System.out.println("Result is : " + result.getText());
+        System.out.println("Calculation is completed");
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+            calculatorOpen();
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println(e.getMessage());
+            System.out.println(e.getCause());
+            System.out.println(e.getStackTrace());
+            System.out.println("Device not found");
+
+        }
     }
 }
